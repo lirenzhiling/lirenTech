b6e3701cf3cac3e74fe67fbb02b642a274685d49
lirenzhiling <2430605716@qq.com>
2024-08-13 23:09:58 +0800

    elementPlus自动按需引入配置

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 
+// elementPlus按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -8,6 +13,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   plugins: [
     vue(),
+    // ...
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      // 配置elementPlus采用sass样式配色系统
+      resolvers: [ElementPlusResolver({ importStyle: "sass" })],
+    }),
     vueDevTools(),
   ],
   resolve: {
