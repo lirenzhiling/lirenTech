9114b3bdfb4fd3dc92d2104e92a2570648eef8fd
lirenzhiling <2430605716@qq.com>
2024-08-16 20:31:56 +0800

    静态资源引入和插件安装

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,11 +5,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// 引入初始化样式文件
+import "@/styles/common.scss";
+
+// 测试接口函数
 import { getCategory } from "@/apis/testAPI";
 getCategory().then(res => {
   console.log(res);
 })
 const app = createApp(App)
+
 
 app.use(createPinia())
 app.use(router)
