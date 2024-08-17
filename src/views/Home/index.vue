4f874fbce5ad6a0fbf57724fc00a5dd37092e276
lirenzhiling <2430605716@qq.com>
2024-08-18 00:00:04 +0800

    home-整体结构拆分以及分类的数据渲染

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,7 +1,17 @@
-<template>
-  <div>我是home页面</div>
-  <div style="height: 1200px;"></div>
-</template>
-<script setup >
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import HomeProduct from './components/HomeProduct.vue'
 </script>
-<style scoped></style>
+
+<template>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <HomeProduct />
+</template>
